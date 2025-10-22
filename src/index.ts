4ee140517aad27e66c9d589b939ffea45a9c0a45
goobja-lang <goobja@naver.com>
2025-10-22 10:17:10 +0900

    .

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { AppDataSource } from "./data-source1.js";
+
+const app = new Hono();
+
+/** DB 연결 */
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
+/** DB 연결 END */
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
